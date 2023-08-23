d828d43e6121fa1b8d306263657f82d277650fb9
Meghan Borg <141247694+meghan-borg@users.noreply.github.com>
2023-08-23 12:53:14 -0400

    reformat

--- unused_commands.R
+++ unused_commands.R
@@ -1,3 +1,4 @@
 #### UNUSED COMMANDS ####
 
 # https://docs.moodle.org/402/en/Markdown#:~:text=Bullet%20point%20lists%20can%20be,and%20should%20not%20be%20forgotten.
+# https://cran.r-project.org/web/packages/vitae/vignettes/vitae.html
